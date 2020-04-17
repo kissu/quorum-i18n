60f11395596d2b4cb1cbf5c8ba5d782d3d303b3b
Konstantin BIFERT <konstantin.bifert@gmail.com>
2020-04-17 18:46:16 +0200

    try custom bump

---
+++ custom_bump.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+VERSION=`git tag | sort -V | tail -1 | cut -f2 -d 'v'`
+if [ -z "$VERSION" ]; then
+  git tag "v1"
+else
+  OLD_VERSION_NUMBER=(${VERSION//v/})
+  echo "Old tag version number: $OLD_VERSION_NUMBER"
+  NEW_VERSION_NUMBER=$(($OLD_VERSION_NUMBER + 1))
+  git tag "v$NEW_VERSION_NUMBER"
+fi
+git push --tags
+echo "New tag version created and pushed"
