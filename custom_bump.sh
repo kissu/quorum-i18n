d9b8f148fb87f062b6dfd0ab701ac18b818e3280
Konstantin BIFERT <konstantin.bifert@gmail.com>
2020-04-18 14:30:13 +0200

    again

--- custom_bump.sh
+++ custom_bump.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
-eval $(ssh-agent -s)
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-
-git config --global user.name TastyCI
-git config --global user.email hello@kissu.io
-git remote add origin git@gitlab.com:kissu/test_weblate.git
-
 VERSION=`git tag | sort -V | tail -1 | cut -f2 -d 'v'`
 echo "nice: $VERSION"
 if [ -z "$VERSION" ]; then
