bc06691ef8d1ac35a189b7b60809a34f9c4f1577
Konstantin BIFERT <konstantin.bifert@gmail.com>
2020-04-24 12:10:20 +0200

    try out a new version

--- testMerge.js
+++ testMerge.js
@@ -3,9 +3,10 @@ const fs = require('fs-extra')
 // merge various JSONs
 const deepExtend = require('deep-extend')
 
-const first = require('./initial-locales/en.json')
-const second = require('./initial-locales/fr.json')
+// const first = require('./initial-locales/en.json')
+const fr = require('./initial-locales/fr.json')
+const canadian = require('./initial-locales/canadian.json')
 
-let mergedJsons = deepExtend(first, second)
+let mergedJsons = deepExtend(fr, canadian)
 
 fs.outputJsonSync('./merged-locales/message2.json', mergedJsons)
