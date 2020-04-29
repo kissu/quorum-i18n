abbc378fbc4f8bb9e47b7b7129e1a099acf42080
Konstantin BIFERT <konstantin.bifert@gmail.com>
2020-04-29 12:20:40 +0200

    chore: update logic and rename

---
+++ createLanguageFallbacks.js
@@ -0,0 +1,26 @@
+const fs = require('fs-extra') // beter file creation
+const deepExtend = require('deep-extend') // merge various JSONs
+
+/* eslint-disable */
+const allLanguagesVariants = [
+  ['fr'],
+  ['fr', 'canadian'],
+  ['canadian', 'chelou'],
+  ['fr', 'canadian', 'chelou'],
+  ['en'],
+  ['en', 'gb']
+]
+/* eslint-enable */
+
+const jsonFileName = (languageFallbackList) =>
+  `./merged-locales/${languageFallbackList.map((lang) => lang.slice(0, 4)).join('-')}.json`
+const importFile = (lang) => require(`./initial-locales/${lang}.json`)
+
+for (const languageVariants of allLanguagesVariants) {
+  const importedLanguageLocaleToMerge = languageVariants.reduce((accumulator, currentValue) => {
+    accumulator.push(importFile(currentValue))
+    return accumulator
+  }, [])
+  let mergedJson = deepExtend(...importedLanguageLocaleToMerge)
+  fs.outputJsonSync(jsonFileName(languageVariants), mergedJson)
+}
