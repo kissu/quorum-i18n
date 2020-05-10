a0cb1f6028674e5d17217500b35cde503fc9d958
Konstantin BIFERT <konstantin.bifert@gmail.com>
2020-05-10 15:19:42 +0200

    not working atm

---
+++ allLanguageVariants.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+const allLanguagesVariants = [
+  ['en'],
+  ['en', 'ong'],
+  ['en', 'ong', 'vert'],
+  ['fr'],
+  ['fr', 'pltk'],
+  ['fr', 'pltk', 'larm'],
+  ['test', 'pltk', 'larm']
+]
+/* eslint-enable */
+export { allLanguagesVariants }
--- createLanguageFallbacks.js
+++ createLanguageFallbacks.js
@@ -1,27 +1,21 @@
-const fs = require('fs-extra') // beter file creation
-const deepExtend = require('deep-extend') // merge various JSONs
-
-/* eslint-disable */
-const allLanguagesVariants = [
-  ['en'],
-  ['en', 'ong'],
-  ['en', 'ong', 'vert'],
-  ['fr'],
-  ['fr', 'pltk'],
-  ['fr', 'pltk', 'larm'],
-  ['test', 'pltk', 'larm']
-]
-/* eslint-enable */
+import fs from 'fs-extra' // beter file creation
+import deepExtend from 'deep-extend' // merge various JSONs
+import { allLanguagesVariants } from './allLanguageVariants.js'
 
 const jsonFileName = (languageFallbackList) =>
   `./merged-locales/${languageFallbackList.map((lang) => lang.slice(0, 4)).join('-')}.json`
-const importFile = (lang) => require(`./initial-locales/${lang}.json`)
+const importFile = async (lang) => {
+  const module = await import(`./initial-locales/${lang}.json`).catch((e) => console.log(e))
+  console.log('nice', module)
+  return module.default
+}
+// await import(`quorum-i18n/merged-locales/${locale}.json`)
 
 for (const languageVariants of allLanguagesVariants) {
-  const importedLanguageLocaleToMerge = languageVariants.reduce((accumulator, currentValue) => {
-    accumulator.push(importFile(currentValue))
+  const importedLanguageLocaleToMerge = languageVariants.reduce(async (accumulator, currentValue) => {
+    accumulator.push(await importFile(currentValue))
     return accumulator
   }, [])
-  let mergedJson = deepExtend(...importedLanguageLocaleToMerge)
+  let mergedJson = deepExtend(Array.from(importedLanguageLocaleToMerge))
   fs.outputJsonSync(jsonFileName(languageVariants), mergedJson)
 }
