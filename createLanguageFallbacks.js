d79dbd519ba9fe6ebafea682576b95ae96a78a6f
Konstantin BIFERT <konstantin.bifert@gmail.com>
2020-05-22 14:22:29 +0200

    update locales

--- createLanguageFallbacks.js
+++ createLanguageFallbacks.js
@@ -9,16 +9,22 @@ const allLanguagesVariants = [
   ['en', 'pltk'],
   ['en', 'pltk', 'larm'],
 
-  ['fr'],
-  ['fr', 'ong'],
-  ['fr', 'ong', 'vert'],
-  ['fr', 'pltk'],
-  ['fr', 'pltk', 'larm'],
+  ['en', 'en_GB'],
+  ['en', 'en_GB', 'ong'],
+  ['en', 'en_GB', 'ong', 'vert'],
+  ['en', 'en_GB', 'pltk'],
+  ['en', 'en_GB', 'pltk', 'larm'],
+
+  ['en', 'fr'],
+  ['en', 'fr', 'ong'],
+  ['en', 'fr', 'ong', 'vert'],
+  ['en', 'fr', 'pltk'],
+  ['en', 'fr', 'pltk', 'larm'],
 ]
 /* eslint-enable */
 
 const jsonFileName = (languageFallbackList) =>
-  `./merged-locales/${languageFallbackList.map((lang) => lang.slice(0, 4)).join('-')}.json`
+  `./merged-locales/${languageFallbackList.map((lang) => lang.match(/\w+/gi)[0]).join('-')}.json`
 const importFile = (lang) => require(`./initial-locales/${lang}.json`)
 
 for (const languageVariants of allLanguagesVariants) {
