99067ecbfb456f0b6398de172a3a9fc97ebc95da
YoshiYo <guillaume.forgue@gmail.com>
2020-05-28 18:53:19 +0200

    fix: repair the switch on mobile retrieve file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 var func = require('../dist/index.js')
 
 async function test() {
-  const module = await func.getJSONLanguageForApplications('fr', 'mediation', 'web')
-  console.log(module.content._COMMON)
+  const module = await func.getJSONLanguageForApplications('fr', 'mediation', 'mobile')
+  console.log(module.content)
 }
 test()
