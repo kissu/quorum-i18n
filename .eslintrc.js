bc21e5804a6bd69edc071d4407997b8e78f4d110
Konstantin BIFERT <konstantin.bifert@gmail.com>
2020-05-28 17:29:07 +0200

    misc: build setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,21 @@
 module.exports = {
-	root: true,
-	env: {
-		browser: true,
-		es6: true,
-		node: true,
-	},
-	extends: [
-		'plugin:prettier/recommended',
-		'eslint:recommended',
-	],
-	parserOptions: {
-		parser: 'babel-eslint',
-		ecmaVersion: 2020,
-		sourceType: 'module',
-	},
-	rules: {
-		'no-console': ['warn', { allow: ['warn', 'debug'] }],
-		'no-warning-comments': [
-			'warn',
-			{ terms: ['todo', 'debug', 'fixme'], location: 'anywhere' },
-		],
-		'no-mixed-spaces-and-tabs': 'warn',
-		'no-empty-pattern': 'warn',
-		'no-unused-vars': 'warn',
-	},
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['plugin:prettier/recommended', 'eslint:recommended'],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'debug'] }],
+    'no-warning-comments': ['warn', { terms: ['todo', 'debug', 'fixme'], location: 'anywhere' }],
+    'no-mixed-spaces-and-tabs': 'warn',
+    'no-empty-pattern': 'warn',
+    'no-unused-vars': 'warn',
+  },
 }
